c51da2caf8f1e985e59f27e6de74e002f33fe7bd
Abdullah Saeed <165245683+Abdullah-Saeed-BB@users.noreply.github.com>
2024-09-16 15:04:52 +0300

    Add files via upload

---
+++ insertionSort.js
@@ -0,0 +1,22 @@
+function insertionSort(array) {
+    let result = array
+  
+    mainLoop: for (let i1 = 1; i1 < array.length; i1++) {
+      if (result[i1] < result [i1 - 1]) {
+        for (let i2 = i1 - 1; i2 >= 0; i2--) {
+          if (result[i1] > result[i2]) {
+            result = [...result.slice(0, i2 + 1), array[i1], ...result.filter((_, i) => i !== i1).slice(i2 + 1)]
+            continue mainLoop
+          } else if (i2 === 0) {
+            result = [array[i1], ...result.filter((_, i) => i !== i1)]
+          }
+        }
+      }
+    }
+    
+    return result;
+  }
+  
+  
+  
+  console.log(insertionSort([5, 4, 33, 2, 8]))
